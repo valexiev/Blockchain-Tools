3ed9194afb4b082bde45aff623b93131667cd878
ivan3a <email9410@abv.bg>
2018-02-26 23:55:44 +0200

    Add transaction sign

--- wallet/models/Wallet.js
+++ wallet/models/Wallet.js
@@ -3,6 +3,7 @@ const ed = require('ed25519-supercop')
 const pbkdf2 = require('pbkdf2');
 const Mnemonic = require('bitcore-mnemonic');
 const cryptoUtil = require('../utils/cryptoUtils'); 
+const Transaction = require('../../node/models/transaction');
 
 class Wallet {
 	constructor(){
@@ -19,7 +20,7 @@ class Wallet {
 		// input : sha256("bitcoin")
 		//output : 6842e6aec8158a7b81ab1c46dd2f9fc27a320ff870ac2fbcb8ae990e341be94f7da2d047c1457df2ef1302820267e2eab98e945691d72085d87f2978790e4077
 		
-		this.secret = pbkdf2.pbkdf2Sync(this.password, 'peper', 1, 32, 'sha256');
+		this.secret = pbkdf2.pbkdf2Sync(this.password, 'peper', 10, 32, 'sha256').toString('hex');
 		return this.secret;
 	}
 	
@@ -33,7 +34,7 @@ class Wallet {
 		let wallet = new Wallet();
 		wallet.id = cryptoUtil.randomId();
 		wallet.password = mnemonic;
-		wllet.mnemonic = mnemonic;
+		wallet.mnemonic = mnemonic;
 		return wallet;
 	}
 	
@@ -44,19 +45,33 @@ class Wallet {
 		
 		let lastKeyPair = this.keyPairs[this.keyPairs.length - 1];
 		
-		let seed = (lastKeyPair == null) ? this.secret : this.generateSecret();
+		//let seed = (lastKeyPair == null) ? this.secret : this.generateSecret();
+		let seed = ed.createSeed()
 		
 		let keyPair = ed.createKeyPair(seed);
-		
+	
 		let newKeyPair = {
 			index : this.keyPairs.length + 1,
-			secretKey : keyPair.secretKey,
+			secretKey : keyPair.secretKey.toString('hex'),
 			publicKey : keyPair.publicKey
 		}
-		
+
 		this.keyPairs.push(newKeyPair);
 		
 		return newKeyPair.publicKey;
+	}
+
+	createTransaction(to,amount) {
+		this.generateAddress();
+		let lastKeyPairIndex = this.keyPairs.length;
+		let from = this.keyPairs[lastKeyPairIndex - 1];
+	
+		let new_transaction = new Transaction(from.publicKey,to,amount);
+		
+		let signature = ed.sign("message",from.publicKey,from.secretKey).toString('hex');
+
+		let isValid = ed.verify(signature, "message" , from.publicKey);
+		return isValid;
 	}
 	
 	static fromPassword(){
@@ -68,7 +83,6 @@ class Wallet {
 }
 
 let wallet = new Wallet();
-
-wallet.password = '6b88c087247aa2f07ee1c5956b8e1a9f4c7f892a70e324f1bb3d161e05ca107b';
-
-console.log(wallet.generateSecretByMnemonic());
+wallet.password = "password";
+wallet.generateAddress();
+wallet.createTransaction("valio", 1);
