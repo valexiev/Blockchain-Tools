b1962f33f4f2e9222e9847e5b6fb84340c5a25e3
valexiev <ghm@valexiev.com>
2018-03-04 19:59:33 +0200

    Change port variable for ws

--- node/index.js
+++ node/index.js
@@ -7,7 +7,7 @@ const Blockchain = require('./models/blockchain')
 const TransactionsStack = require('./models/transactionsStack')
 
 const httpPort = process.env.PORT || config.httpPort || '5001'
-const wsPort = process.env.PORT || config.wsPort || '8001'
+const wsPort = process.env.WS_PORT || config.wsPort || '8001'
 const nodeURL = config.url + ':' + httpPort
 
 var pendingTransactions = new TransactionsStack()
@@ -31,5 +31,3 @@ WsServer({
 	blockchain,
 	node
 }).startServer()
-
-
