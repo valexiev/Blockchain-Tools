41d489866090d4629deecbf8c56b563e35622588
ivan3a <email9410@abv.bg>
2018-03-04 18:25:03 +0200

    Add wallet that works

--- wallet/models/Wallet.js
+++ wallet/models/Wallet.js
@@ -1,15 +1,20 @@
-const cryptoJS = require('crypto-js');
-const ed = require('ed25519-supercop')
+//const cryptoJS = require('crypto-js');
+const crypto = require('crypto');
+const ed = require('ed25519-supercop');
+const EC = require('elliptic').ec;
+const ec = new EC('secp256k1');
 const pbkdf2 = require('pbkdf2');
 const Mnemonic = require('bitcore-mnemonic');
 const cryptoUtil = require('../utils/cryptoUtils'); 
 const Transaction = require('../../node/models/transaction');
 const request = require('request');
+const fs = require('fs');
+const http = require('http');
 
 class Wallet {
-	constructor(){
+	constructor(password){
 		this.id = null;
-		this.password = null;
+		this.password = password;
 		this.secret = null;
 		this.keyPairs = [];
 		this.mnemonic = null;
@@ -54,28 +59,57 @@ class Wallet {
 	
 		let newKeyPair = {
 			index : this.keyPairs.length + 1,
-			secretKey : keyPair.secretKey.toString('hex'),
-			publicKey : keyPair.publicKey
+			privateKey : keyPair.secretKey.toString('hex'),
+			publicKey : keyPair.publicKey.toString('hex')
 		}
 
 		this.keyPairs.push(newKeyPair);
 		
 		return newKeyPair.publicKey;
 	}
+	
+	keyPairToFile(){
+		this.generateAddress(); //generate keypair
+		let fileName = ed.createSeed().toString('hex');
+		let stream = fs.createWriteStream("../app/keypairs/" + fileName + ".json");
+		let lastKeyPair = this.keyPairs[this.keyPairs.length - 1];
+		
+		stream.once('open', function(fd) {
+		  stream.write(JSON.stringify(lastKeyPair));
+		  stream.end();
+		});
+		
+		return '/keypairs/' + fileName + '.json';
+	}
 
-	createTransaction(to,amount) {
+	createTransaction(to,amount,privateKey) {
 		this.generateAddress();
 		let lastKeyPairIndex = this.keyPairs.length;
 		let from = this.keyPairs[lastKeyPairIndex - 1];
 	
-		let message = from.publicKey + "-" + to + "amount";
-		let signature = ed.sign(message, from.publicKey, from.secretKey).toString('hex');
+		let message = from.publicKey + to + amount;
+		let signature = ed.sign(message, from.publicKey, privateKey).toString('hex');
+
 
 		return new Transaction(from.publicKey,to,amount,message,signature);
 	}
 
-	function sendTransaction(transaction){
+	sendTransaction(transaction){
 		// send post request to this.nodeUrl
+		// http://192.168.87.96:5001/
+
+		request.post('http://192.168.87.96:5001/transactions', { json: { transaction: transaction } },
+	    	function (error, response, body) {
+		        if (!error && response.statusCode == 200) {
+		            console.log(body);
+		        }
+		    }
+		);
+
+	}
+
+	walletFromPrivateKey(privateKey){
+
 	}
 	
 	static fromPassword(){
@@ -85,10 +119,5 @@ class Wallet {
 		return wallet;
 	}
 }
-
-//let wallet = new Wallet();
-//wallet.password = "password";
-//wallet.generateAddress();
-//wallet.createTransaction("valio", 1);
 
 module.exports = Wallet;
