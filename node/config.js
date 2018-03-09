a4d18186abe8187a398dd355de6fd08720272137
valexiev <ghm@valexiev.com>
2018-03-09 16:31:20 +0200

    Add config addresses

--- node/config.js
+++ node/config.js
@@ -3,5 +3,7 @@ module.exports = {
     httpPort: 5001,
     wsPort: 8001,
     nodeName: 'nodeName',
-    initialPeers: []
+    initialPeers: [],
+    miningAddress: '123',
+    faucetAddress: 'd55cb1e70eaba2dd591cd3c75cd518745a5c2f99f5f3e07c281ca0229edc1445'
 }
