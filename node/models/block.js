91243c32ac531e48b18eb7ca888bd781fc94cddb
valexiev <ghm@valexiev.com>
2018-03-09 22:25:03 +0200

    Remove unused Block method

--- node/models/block.js
+++ node/models/block.js
@@ -20,20 +20,6 @@ class Block {
 		this.blockHash = this.toHash();
 	}
 
-	static get genesisBlock(){
-		//TODO : add transaction with coins to faucet
-		return new Block(
-			0, // block index
-			[], //transactions
-			4, //difficulty
-			'0', //previous block hash
-			'Satoshi', // block miner
-			1518342536, // current timestamp
-			'e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855', // block hash
-			123, //nonce
-		);
-	}
-
 	getTransactions() {
 		return this.transactions
 	}
